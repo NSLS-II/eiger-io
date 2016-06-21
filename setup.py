efa3cf32fbdf5511d0f1d68f1011c4ba15ef272f
Eric Dill <edill@bnl.gov>
2016-06-21 15:15:50 -0400

    Add versioneer to use git for version info

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup
+import versioneer
 
+setup(version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
+      name='eiger_io',
+      packages=['eiger_io'])
 
-setup(name='eiger_io',
-      packages=['eiger_io'],
-      version='0.0.2')
